d38c765a0d7ca77690bf586379d219429025de28
Adam Guest <chamook@chamook.dev>
2019-09-25 14:40:33 +0200

    Add a simple route for getting an entry by id

--- src/Chamook.Sample/Routing.fs
+++ src/Chamook.Sample/Routing.fs
@@ -2,9 +2,23 @@ module Routing
 
 open System
 open Giraffe
+open Microsoft.AspNetCore.Http
+open FSharp.Control.Tasks.V2.ContextInsensitive
+
+let getSingleEntry id =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! entry = Database.readEntry id
+
+            return! negotiate entry next ctx
+        }
 
 let webApp: HttpHandler =
     choose [
-        GET >=> route "/health" >=> text "Everything's fine here"
+        GET
+        >=> choose [
+            route "/health" >=> text "Everything's fine here"
+            routef "/entries/%s" getSingleEntry
+            ]
         RequestErrors.NOT_FOUND "Damn son"
     ]
